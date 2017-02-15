098f49d640db76d262d83f64fc5563c590515009
generalmeow <rcbrown@gmail.com>
2017-02-14 18:22:43 -0800

    WIP fixing routes

--- queries.js
+++ queries.js
@@ -2,8 +2,6 @@ const pgp = require('pg-promise')()
 const connectionString = 'postgres://localhost:5432/sweethornbillbookstore'
 const db = pgp(connectionString)
 
-
-
 const getAllBooks = () => db.any( 'SELECT * FROM books' )
 
 const getSingleBook = ( request, response ) => {
@@ -13,8 +11,8 @@ const getSingleBook = ( request, response ) => {
 
 const getSingleAuthor = ( request, response ) => {
   const author = request.params.author.toLowerCase()
-    console.log(author)
-    return db.one('SELECT * FROM books WHERE LOWER(author) = $1', author)
+  console.log('author===>', author)
+    return db.any('SELECT * FROM books WHERE LOWER(author) = $1', author)
 }
 
 const getSingleGenre = ( request, response ) => {
@@ -22,13 +20,13 @@ const getSingleGenre = ( request, response ) => {
     return db.one('SELECT * FROM books WHERE genre = $1', genre)
 }
 
-const addBook = ( request, response ) => {
+const addBookPage = ( request, response ) => {
   return db.any('SELECT * FROM books')
 }
 
 const createBook = ( request, response ) => {
   const { title, author, genre, description, img_url } = request.body
-    return db.any('INSERT INTO books( title, author, genre, description, img_url) VALUES( $1, $2, $3, $4, $5 ) RETURNING *')
+    return db.any('INSERT INTO books( title, author, genre, description, img_url) VALUES( $1, $2, $3, $4, $5 ) RETURNING *', [ title, author, genre, description, img_url ] )
 }
 
 
@@ -38,6 +36,6 @@ module.exports = {
   getSingleBook,
   getSingleAuthor,
   getSingleGenre,
-  addBook,
+  addBookPage,
   createBook
 }
